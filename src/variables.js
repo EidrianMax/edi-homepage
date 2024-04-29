b0de79d3b66978b76505e97d85b69d62262ea66f
Adrian Alvarado <eidrian.uct@gmail.com>
2024-04-29 14:28:36 +0200

    finish proyect

---
+++ src/variables.js
@@ -0,0 +1,9 @@
+export const variables = {
+  colors: {
+    black: '#333333',
+    white: '#F2F2F2',
+    lightBlue: '#2D9CDB',
+    blackBlue: '#100E1D'
+  },
+  font: `'Poppins', 'sans-serif'`,
+}
